efe699c4592c3dbb7fde25ade9294f0c7fe16fbb
Abadat <abadat496@gmail.com>
2025-01-05 17:31:42 +0530

    gtag

--- pages/components/GoogleTag.js
+++ pages/components/GoogleTag.js
@@ -3,36 +3,47 @@ import Head from "next/head";
 
 const GoogleTag = () => {
   useEffect(() => {
-    const script = document.createElement("script");
+    const script1 = document.createElement("script");
     const script2 = document.createElement("script");
-    script2.src =
-      "https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js";
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=AW-16800279967";
+    script1.async = true;
+    script2.src = "https://www.googletagmanager.com/gtag/js?id=G-CMQSDJ1M0F";
     script2.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-2HEM49C7R4";
-    script.async = true;
 
     // Load the scripts after the initial rendering
-    document.body.appendChild(script);
-    document.body.appendChild(script2);
+    document.head.appendChild(script1);
+    document.head.appendChild(script2);
 
-    script.onload = () => {
+    script1.onload = () => {
+      // Execute your scripts here after the scripts are loaded
       window.dataLayer = window.dataLayer || [];
       function gtag() {
         dataLayer.push(arguments);
       }
       gtag("js", new Date());
 
-      gtag("config", "'G-2HEM49C7R4'");
-      // gtag("config", "AW-16451480682");
+      gtag("config", "G-CMQSDJ1M0F");
+    };
 
-      gtag("config", "AW-16451480682/uolQCLP7tZkZEOrY1qQ9", {
-        phone_conversion_number: "+1 (855) 550-0742",
+    script2.onload = () => {
+      // Execute your scripts here after the scripts are loaded
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "AW-16800279967");
+
+      gtag("config", "AW-16800279967/dgNjCJbauIAaEJ_b_8o-", {
+        phone_conversion_number: "800 461 0995",
       });
     };
 
     // Clean up the script element when the component is unmounted
     return () => {
-      document.body.removeChild(script);
+      document.head.removeChild(script1);
+      document.head.removeChild(script2);
     };
   }, []);
 
